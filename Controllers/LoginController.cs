a32a7c86f9ed10fa506f432fc976a4f0c45c71f1
Ziv-Faigenboim <ziv.lehavim@gmail.com>
2024-02-19 19:21:37 +0200

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+//using Oracle.ManagedDataAccess.Client; // If using Managed ODP.NET
+
+namespace ShopProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("LoginPage");
+        }
+    }
+}
+
---
+++ Models/UserRegister.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopProject.Models
+{
+    public class UserRegister
+    {
+        [Key]
+        public int UserID { get; set; }
+        [Required(ErrorMessage ="First Name is required.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
