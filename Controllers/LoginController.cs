721138381b7981481a4fd704c0866671da209236
Ziv-Faigenboim <ziv.lehavim@gmail.com>
2024-02-19 21:28:15 +0200

    kjj

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -16,23 +16,46 @@ namespace ShopProject.Controllers
     public class LoginController : Controller
     {
         private readonly IConfiguration _configuration;
-        string connectionString = "";
+        private string connectionString="";
         public LoginController(IConfiguration configuration)
         {
             _configuration = configuration;
-            connectionString = _configuration.GetConnectionString("myConnect");
+            connectionString = _configuration.GetConnectionString("MyConnectionString");
 
         }
         public IActionResult Index()
         {
             UsersModel user = new UsersModel();
             return View("LoginPage", user);
+
         }
         [Route("ShowDetails")]
         public IActionResult ShowDetails(UsersModel user)
         {
+            connection.Open();
+            string sqlQuery = "INSERT INTO USERS VALUES(@value1,@value2)";
+            using (SqlConnection connection = new SqlCommand(sqlQuery, connection))
+            
+            
+            
+            {
+                command.Parameters.AddWithValue("@value1", user.UserName);
+                command.Parameters.AddWithValue("@value2", user.Password);
+                int rowsAffected = command.ExecuteNonQuery();
+
+                return View("ShowDetails", user);
+
+
+
+            }
             return View("ShowDetails", user);
         }
+
+       // public IActionResult AddUserToDatabase(UsersModel user)
+        //{
+            
+        //}
+
     }
 }
 
