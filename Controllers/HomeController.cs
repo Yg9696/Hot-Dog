01bd830d95dc5f2b392e557a8531e179ff809d3f
YonatanGuetta <yonatanguetta9696@gmail.com>
2024-03-13 15:47:06 +0200

    fgfgf

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,21 +8,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using System.Text.Json;
+using ShopProject.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace ShopProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ShopService shop;
+        private readonly IConfiguration _configuration;
+        private readonly string connectionString;
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
+            _configuration = configuration;
+            shop = new ShopService(_configuration);
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+           
+            return View(shop.GetListOf("Products").Cast<ProductsModel>().ToList());
         }
 
         public IActionResult Privacy()
